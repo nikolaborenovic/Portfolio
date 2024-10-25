bf9980c95f07bfd3e7dc4e543a513fa015fa7333
nikolaborenovic <n97.borenovic@gmail.com>
2024-10-25 18:15:38 +0200

    Finishing navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navigation from './features/Navigation/Navigation'
+import Home from './features/Home/Home'
+import About from './features/About/About'
+import Works from './features/Works/Works'
+import Contact from './features/Contact/Contact'
 
 function App() {
   
 
   return (
     <>
-     <p>Nesto</p>
-     <Navigation/>
+     <BrowserRouter>
+      <Navigation/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/works' element={<Works/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- src/features/Navigation/Navigation.jsx
+++ src/features/Navigation/Navigation.jsx
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom';
 import './navigation.style.css'
 
 const Navigation = () => {
 
     return(
-        <p>Navigation</p>
+        <div className='portfolio_navigation'>
+            <Link to={{pathname: '/'}}>Home</Link>
+            <Link to={{pathname: '/about'}}>About</Link>
+            <Link to={{pathname: '/works'}}>Works</Link>
+            <Link to={{pathname: '/contact'}}>Contact</Link>
+        </div>
     )
 }
 
