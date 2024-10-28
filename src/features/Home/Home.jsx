3e4d60787f0f5fc2e0c3be0b296bdc6cc4181448
nikolaborenovic <n97.borenovic@gmail.com>
2024-10-28 20:49:32 +0100

    Home section creation

--- src/features/Home/Home.jsx
+++ src/features/Home/Home.jsx
@@ -1,8 +1,15 @@
 import './home.style.css'
+import profilePhoto from "../../assets/profile.jpg"
+import SocialNetwork from './components/SocialNetwork';
 
 const Home = () => {
     return(
-        <div>Home</div>
+        <div className='profile_card'>
+            <img className='profile_image' src={profilePhoto}/>
+            <h1>Hi, I am <span>Nikola Borenovic</span></h1>
+            <p>I am a frontend web developer. I can provide clean code and pixel perfect design. I also make website more and more interactive with web animations.</p>
+            <SocialNetwork/>
+        </div>
     )
 }
 
